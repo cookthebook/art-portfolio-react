53f24e71705b67eda96a66ee33f9091e385f4ab9
Aaron Cook <aarongregorycook@gmail.com>
2018-06-25 11:03:10 -0500

    Refactoring to 2 space tabs

--- src/App.js
+++ src/App.js
@@ -9,80 +9,80 @@ import './App.css';
 
 class App extends Component {
   constructor (props) {
-    super(props);
-    this.images_dig_drawing = [
-      { image: new ImageContainer("22.jpg", "Artesynal"), key: "23" },
-      { image: new ImageContainer("20.jpg", "Android 21"), key: "22" },
-      { image: new VideoContainer("bust_a_move.jpg", "Bust a Move", "bust_a_move.mp4"), key: "21"},
-      { image: new VideoContainer("hmmm.jpg", "Hmmm", "hmmm.mp4"), key: "20" },
-      { image: new ImageContainer("18.jpg", "Paarthunax"), key: "18" },
-      { image: new ImageContainer("17.jpg", "Mononoke"), key: "17" },
-      { image: new ImageContainer("16.jpg", "Gator"), key: "16" },
-      { image: new ImageContainer("15.jpg", "Mad Max"), key: "15" },
-    ];
+  super(props);
+  this.images_dig_drawing = [
+    { image: new ImageContainer("22.jpg", "Artesynal"), key: "23" },
+    { image: new ImageContainer("20.jpg", "Android 21"), key: "22" },
+    { image: new VideoContainer("bust_a_move.jpg", "Bust a Move", "bust_a_move.mp4"), key: "21"},
+    { image: new VideoContainer("hmmm.jpg", "Hmmm", "hmmm.mp4"), key: "20" },
+    { image: new ImageContainer("18.jpg", "Paarthunax"), key: "18" },
+    { image: new ImageContainer("17.jpg", "Mononoke"), key: "17" },
+    { image: new ImageContainer("16.jpg", "Gator"), key: "16" },
+    { image: new ImageContainer("15.jpg", "Mad Max"), key: "15" },
+  ];
 
-    this.images_personal = [
-      { image: new ImageContainer("24.jpg", "Super Girl"), key:"25" },
-      { image: new ImageContainer("21.jpg", "Ace of Hearts"), key: "24" },
-      { image: new ImageContainer("19.jpg", "Daniel Bamdad"), key: "19" },
-      { image: new ImageContainer("14.jpg", "Bitterblossom"), key: "14" },
-      { image: new ImageContainer("13.jpg", "Good Joe"), key: "13" },
-      { image: new ImageContainer("12.jpg", "Lunara"), key: "12" },
-      { image: new ImageContainer("11.jpg", "Eye"), key: "11" },
-      { image: new ImageContainer("10.jpg", "Arya"), key: "10" },
-    ];
+  this.images_personal = [
+    { image: new ImageContainer("24.jpg", "Super Girl"), key:"25" },
+    { image: new ImageContainer("21.jpg", "Ace of Hearts"), key: "24" },
+    { image: new ImageContainer("19.jpg", "Daniel Bamdad"), key: "19" },
+    { image: new ImageContainer("14.jpg", "Bitterblossom"), key: "14" },
+    { image: new ImageContainer("13.jpg", "Good Joe"), key: "13" },
+    { image: new ImageContainer("12.jpg", "Lunara"), key: "12" },
+    { image: new ImageContainer("11.jpg", "Eye"), key: "11" },
+    { image: new ImageContainer("10.jpg", "Arya"), key: "10" },
+  ];
 
-    this.images_drawing = [
-      { image: new ImageContainer("9.jpg", "Self"), key: "9" },
-      { image: new ImageContainer("4.jpg", "Wolf"), key: "4" },
-      { image: new ImageContainer("3.jpg", "Deer"), key: "3" },
-      { image: new ImageContainer("2.jpg", "Hawk"), key: "2" },
-      { image: new ImageContainer("1.jpg", "Goat"), key: "1" },
-    ];
+  this.images_drawing = [
+    { image: new ImageContainer("9.jpg", "Self"), key: "9" },
+    { image: new ImageContainer("4.jpg", "Wolf"), key: "4" },
+    { image: new ImageContainer("3.jpg", "Deer"), key: "3" },
+    { image: new ImageContainer("2.jpg", "Hawk"), key: "2" },
+    { image: new ImageContainer("1.jpg", "Goat"), key: "1" },
+  ];
 
-    this.images_all = this.images_dig_drawing.concat(this.images_personal, this.images_drawing);
-    this.images_all.sort((element1, element2) => {
-      if (parseInt(element1.key, 10) > parseInt(element2.key, 10)) {
-        return 1;
-      } else if (parseInt(element1.key, 10) < parseInt(element2.key, 10)) {
-        return -1;
-      } else {
-        return 0;
-      }
-    });
-    this.images_all.reverse();
+  this.images_all = this.images_dig_drawing.concat(this.images_personal, this.images_drawing);
+  this.images_all.sort((element1, element2) => {
+    if (parseInt(element1.key, 10) > parseInt(element2.key, 10)) {
+    return 1;
+    } else if (parseInt(element1.key, 10) < parseInt(element2.key, 10)) {
+    return -1;
+    } else {
+    return 0;
+    }
+  });
+  this.images_all.reverse();
   }
 
   render() {
-    const Home = () => {
-      return (
-        <div>
-          <GridGallery images={this.images_personal} title="Personal (2016-Present)" />
-          <GridGallery images={this.images_dig_drawing} title="UMN Digital Drawing (Spring 2018)" />
-          <GridGallery images={this.images_drawing} title="UMN Intro Drawing (Fall 2016)" />
-        </div>
-      );
-    };
-
-    const Gallery = ({ match }) => {
-      return (
-        <FocusGallery images={this.images_all} match={match} />
-      );
-    };
-
+  const Home = () => {
     return (
-      <BrowserRouter>
-        <div>
-          <Sidebar />
-          <div className="App">
-            <Switch>
-              <Route exact path={"/"} component={Home} />
-              <Route path="/gallery/:key" component={Gallery}/>
-            </Switch>
-          </div>
-        </div>
-      </BrowserRouter>
+    <div>
+      <GridGallery images={this.images_personal} title="Personal (2016-Present)" />
+      <GridGallery images={this.images_dig_drawing} title="UMN Digital Drawing (Spring 2018)" />
+      <GridGallery images={this.images_drawing} title="UMN Intro Drawing (Fall 2016)" />
+    </div>
     );
+  };
+
+  const Gallery = ({ match }) => {
+    return (
+    <FocusGallery images={this.images_all} match={match} />
+    );
+  };
+
+  return (
+    <BrowserRouter>
+    <div>
+      <Sidebar />
+      <div className="App">
+      <Switch>
+        <Route exact path={"/"} component={Home} />
+        <Route path="/gallery/:key" component={Gallery}/>
+      </Switch>
+      </div>
+    </div>
+    </BrowserRouter>
+  );
   }
 }
 
--- src/Gallery.js
+++ src/Gallery.js
@@ -8,181 +8,181 @@ import { CardDeck, Pagination, PaginationItem, ListGroup, ListGroupItem } from '
 import "./Gallery.css";
 
 export const GridGallery = (props) => {
-    const { images } = props;
-    const row_width = 4;
+  const { images } = props;
+  const row_width = 4;
 
-    var img_list = [];
-    images.forEach((element, index) => {
-        img_list.push(
-            <Link to={"/gallery/" + element.key} className="GridElement" key={index} >
-                <ImagePreview image={element.image} />
-            </Link>
-        );
-    });
-
-    var decks = [];
-    for (var y = 0; y < img_list.length/row_width; y++) {
-        var curr_row = [];
-        for (var x = 0; x < row_width; x++) {
-            curr_row.push(img_list[y*row_width + x]);
-        }
-        decks.push(
-            <div className="row justify-content-center" key={y.toString()}>
-                <CardDeck>
-                    {curr_row}
-                </CardDeck>
-            </div>
-        );
-    }
-
-    return (
-        <ListGroup>
-            <ListGroupItem className="GridTitle"><h3 className="GridTitle">{props.title}</h3></ListGroupItem>
-            <ListGroupItem>
-                <div className="Grid">
-                    {decks}
-                </div>
-            </ListGroupItem>
-        </ListGroup>
+  var img_list = [];
+  images.forEach((element, index) => {
+    img_list.push(
+      <Link to={"/gallery/" + element.key} className="GridElement" key={index} >
+        <ImagePreview image={element.image} />
+      </Link>
     );
+  });
+
+  var decks = [];
+  for (var y = 0; y < img_list.length/row_width; y++) {
+    var curr_row = [];
+    for (var x = 0; x < row_width; x++) {
+      curr_row.push(img_list[y*row_width + x]);
+    }
+    decks.push(
+      <div className="row justify-content-center" key={y.toString()}>
+        <CardDeck>
+          {curr_row}
+        </CardDeck>
+      </div>
+    );
+  }
+
+  return (
+    <ListGroup>
+      <ListGroupItem className="GridTitle"><h3 className="GridTitle">{props.title}</h3></ListGroupItem>
+      <ListGroupItem>
+        <div className="Grid">
+          {decks}
+        </div>
+      </ListGroupItem>
+    </ListGroup>
+  );
 }
 
 GridGallery.propTypes = {
-    images: PropTypes.arrayOf(PropTypes.shape({
-        image: PropTypes.instanceOf(ImageContainer),
-        key: PropTypes.string,
-    })).isRequired,
-    title: PropTypes.string.isRequired
+  images: PropTypes.arrayOf(PropTypes.shape({
+    image: PropTypes.instanceOf(ImageContainer),
+    key: PropTypes.string,
+  })).isRequired,
+  title: PropTypes.string.isRequired
 };
 
 export class FocusGallery extends Component {
-    constructor (props) {
-        super(props);
-        this.images = props.images;
+  constructor (props) {
+    super(props);
+    this.images = props.images;
 
-        var index = this.images.findIndex(element => {
-            return (element.key === props.match.params.key);
-        });
+    var index = this.images.findIndex(element => {
+      return (element.key === props.match.params.key);
+    });
 
-        if (index === -1) {
-            index = 0;
-        }
-
-        this.state = {
-            focus: props.match.params.key,
-            index: index,
-        }
-
-        this.change_focus = this.change_focus.bind(this);
+    if (index === -1) {
+      index = 0;
     }
 
-    change_focus (key) {
-        var index = this.images.findIndex(element => {
-            return (element.key === key);
-        });
-
-        if (index === -1) {
-            index = 0;
-        }
-
-        this.setState({
-            focus: key,
-            index: index
-        });
+    this.state = {
+      focus: props.match.params.key,
+      index: index,
     }
 
-    render () {
-        return (
-            <div className="FocusView">
-                <FocusGalleryPagination index={this.state.index} images={this.images} callback={this.change_focus} />
-                <ImageView image={this.images[this.state.index].image} />
-            </div>
-        )
+    this.change_focus = this.change_focus.bind(this);
+  }
+
+  change_focus (key) {
+    var index = this.images.findIndex(element => {
+      return (element.key === key);
+    });
+
+    if (index === -1) {
+      index = 0;
     }
+
+    this.setState({
+      focus: key,
+      index: index
+    });
+  }
+
+  render () {
+    return (
+      <div className="FocusView">
+        <FocusGalleryPagination index={this.state.index} images={this.images} callback={this.change_focus} />
+        <ImageView image={this.images[this.state.index].image} />
+      </div>
+    )
+  }
 }
 
 FocusGallery.propTypes = {
-    images: PropTypes.arrayOf(PropTypes.shape({
-        image: PropTypes.instanceOf(ImageContainer),
-        key: PropTypes.string,
-    })).isRequired
+  images: PropTypes.arrayOf(PropTypes.shape({
+    image: PropTypes.instanceOf(ImageContainer),
+    key: PropTypes.string,
+  })).isRequired
 };
 
 const FocusGalleryPagination = (props) => {
-    const { index, images, callback } = props;
+  const { index, images, callback } = props;
 
-    var first;
+  var first;
 
-    if (index < 3) {
-        first = 0;
-    } else if (index > images.length - 3) {
-        first = images.length - 5;
-    } else {
-        first = index - 2;
-    }
+  if (index < 3) {
+    first = 0;
+  } else if (index > images.length - 3) {
+    first = images.length - 5;
+  } else {
+    first = index - 2;
+  }
 
-    var newer_link;
-    if (index === 0) {
-        newer_link = (
-            <PaginationItem disabled>
-                <div className="page-link">Newer</div>
-            </PaginationItem>
-        );
-    } else {
-        newer_link = (
-            <PaginationItem onClick={() => callback(images[index-1].key)}>
-                <Link to={"/gallery/" + images[index-1].key} className="page-link">
-                    Newer
-                </Link>
-            </PaginationItem>
-        );
-    }
+  var newer_link;
+  if (index === 0) {
+    newer_link = (
+      <PaginationItem disabled>
+        <div className="page-link">Newer</div>
+      </PaginationItem>
+    );
+  } else {
+    newer_link = (
+      <PaginationItem onClick={() => callback(images[index-1].key)}>
+        <Link to={"/gallery/" + images[index-1].key} className="page-link">
+          Newer
+        </Link>
+      </PaginationItem>
+    );
+  }
 
-    var older_link;
-    if (index === (images.length - 1)) {
-        older_link = (
-            <PaginationItem disabled>
-                <div className="page-link">Older</div>
-            </PaginationItem>
-        );
-    } else {
-        older_link = (
-            <PaginationItem onClick={() => callback(images[index+1].key)}>
-                <Link to={"/gallery/" + images[index+1].key} className="page-link">
-                    Older
-                </Link>
-            </PaginationItem>
-        );
-    }
+  var older_link;
+  if (index === (images.length - 1)) {
+    older_link = (
+      <PaginationItem disabled>
+        <div className="page-link">Older</div>
+      </PaginationItem>
+    );
+  } else {
+    older_link = (
+      <PaginationItem onClick={() => callback(images[index+1].key)}>
+        <Link to={"/gallery/" + images[index+1].key} className="page-link">
+          Older
+        </Link>
+      </PaginationItem>
+    );
+  }
 
-    return (
-        <Pagination className="FocusPagination">
-            {newer_link}
-            <PaginationItem className={(first === index) ? "disabled" : ""} onClick={() => callback(images[first].key)}>
-                <Link to={"/gallery/" + images[first].key} className="page-link">{first+1}</Link>
-            </PaginationItem>
-            <PaginationItem className={(first+1 === index) ? "disabled" : ""} onClick={() => callback(images[first+1].key)}>
-                <Link to={"/gallery/" + images[first+1].key} className="page-link">{first+1+1}</Link>
-            </PaginationItem>
-            <PaginationItem className={(first+2 === index) ? "disabled" : ""} onClick={() => callback(images[first+2].key)}>
-                <Link to={"/gallery/" + images[first+2].key} className="page-link">{first+2+1}</Link>
-            </PaginationItem>
-            <PaginationItem className={(first+3 === index) ? "disabled" : ""} onClick={() => callback(images[first+3].key)}>
-                <Link to={"/gallery/" + images[first+3].key} className="page-link">{first+3+1}</Link>
-            </PaginationItem>
-            <PaginationItem className={(first+4 === index) ? "disabled" : ""} onClick={() => callback(images[first+4].key)}>
-                <Link to={"/gallery/" + images[first+4].key} className="page-link">{first+4+1}</Link>
-            </PaginationItem>
-            {older_link}
-        </Pagination>
-    )
+  return (
+    <Pagination className="FocusPagination">
+      {newer_link}
+      <PaginationItem className={(first === index) ? "disabled" : ""} onClick={() => callback(images[first].key)}>
+        <Link to={"/gallery/" + images[first].key} className="page-link">{first+1}</Link>
+      </PaginationItem>
+      <PaginationItem className={(first+1 === index) ? "disabled" : ""} onClick={() => callback(images[first+1].key)}>
+        <Link to={"/gallery/" + images[first+1].key} className="page-link">{first+1+1}</Link>
+      </PaginationItem>
+      <PaginationItem className={(first+2 === index) ? "disabled" : ""} onClick={() => callback(images[first+2].key)}>
+        <Link to={"/gallery/" + images[first+2].key} className="page-link">{first+2+1}</Link>
+      </PaginationItem>
+      <PaginationItem className={(first+3 === index) ? "disabled" : ""} onClick={() => callback(images[first+3].key)}>
+        <Link to={"/gallery/" + images[first+3].key} className="page-link">{first+3+1}</Link>
+      </PaginationItem>
+      <PaginationItem className={(first+4 === index) ? "disabled" : ""} onClick={() => callback(images[first+4].key)}>
+        <Link to={"/gallery/" + images[first+4].key} className="page-link">{first+4+1}</Link>
+      </PaginationItem>
+      {older_link}
+    </Pagination>
+  )
 }
 
 FocusGalleryPagination.propTypes = {
-    index: PropTypes.number.isRequired,
-    images: PropTypes.arrayOf(PropTypes.shape({
-        image: PropTypes.instanceOf(ImageContainer),
-        key: PropTypes.string,
-    })).isRequired,
-    callback: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+  images: PropTypes.arrayOf(PropTypes.shape({
+    image: PropTypes.instanceOf(ImageContainer),
+    key: PropTypes.string,
+  })).isRequired,
+  callback: PropTypes.func.isRequired,
 }
