9a095efe5f2adae1eaa73630ba2811f719c40be7
Aaron Cook <cookx770@umn.edu>
2018-12-10 01:42:53 -0600

    Final change (?)

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ class App extends Component {
       if (expanded) {
         sidebar.style.width = "20px";
         app.style.marginLeft = "20px";
-        hide_button.innerHTML = ">";
+        hide_button.innerHTML = "  ";
 
         for (var i = 0; i < side_links.length; i++) {
           side_links.item(i).style.fontSize = "0px";
