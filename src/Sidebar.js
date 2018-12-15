88fe597309288ce9892aa000e9e5486fa732b5e6
Aaron Cook <cookx770@umn.edu>
2018-12-15 09:03:12 -0600

    Adding horizontal dividers

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,9 +5,17 @@ import './Sidebar.css';
 
 export const Sidebar = (props) => {
   var { hideCallback, links } = props;
+
+  var links_formatted = [];
+  links.forEach(link => {
+    links_formatted.push(link);
+    links_formatted.push(<hr className="SideDivider" />);
+  });
+  links_formatted.pop();
+
   return (
     <div className="Sidebar">
-      {links}
+      {links_formatted}
       <p id="HideButton" className="HideButton" onClick={hideCallback}>{"<"}</p>
     </div>
   );
