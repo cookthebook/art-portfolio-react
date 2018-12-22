2c23b2ba8f81db04adf2cd3b88ef2d543d5146b3
Aaron Cook <cookx770@umn.edu>
2018-12-22 11:18:56 -0600

    Blog posts are more flexible

--- src/Blog.js
+++ src/Blog.js
@@ -4,6 +4,35 @@ import BlogPosts from './blog_posts.json';
 import './Blog.scss';
 
 export class Blog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getBlogBody = this.getBlogBody.bind(this);
+  }
+
+  getBlogBody(bodyElements) {
+    var bodySections = [];
+
+    bodyElements.forEach(element => {
+      switch (element.type) {
+        case "text":
+          bodySections.push(
+            <pre className="BlogPostBody">{element.text}</pre>
+          );
+          break;
+        case "image":
+          bodySections.push(
+            <img src={element.src} style={element.style} />
+          )
+          break;
+        default:
+          break;
+      }
+    })
+
+    return bodySections;
+  }
+
   render() {
     var formatted_posts = [];
 
@@ -12,10 +41,18 @@ export class Blog extends Component {
         <div className="BlogPost">
           <h1 className="BlogPostTitle">{post.title}</h1>
           <p className="BlogPostDate">{post.date}</p>
-          <pre className="BlogPostBody">{post.body}</pre>
+          {this.getBlogBody(post.body)}
         </div>
       )
-    })
+
+      formatted_posts.push(
+        <div style={{"padding-top": "3em", "padding-bottom": "3em"}}>
+          <hr className="PostDivider" />
+        </div>
+      )
+    });
+    formatted_posts.pop();
+    formatted_posts.reverse();
 
     return (
       <div className="BlogContainer">
