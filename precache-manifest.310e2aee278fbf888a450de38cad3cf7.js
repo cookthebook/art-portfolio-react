f2e934ece597fbe32438067ff7523f7da143519b
Aaron Cook <cookx770@umn.edu>
2019-01-09 22:45:34 -0600

    Adding static build

---
+++ precache-manifest.310e2aee278fbf888a450de38cad3cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3fae112e0f739e26bbc",
+    "url": "/static/js/main.d3fae112.chunk.js"
+  },
+  {
+    "revision": "72f3c5d62e55ebe0dba2",
+    "url": "/static/js/1.72f3c5d6.chunk.js"
+  },
+  {
+    "revision": "d3fae112e0f739e26bbc",
+    "url": "/static/css/main.8dbbec05.chunk.css"
+  },
+  {
+    "revision": "72f3c5d62e55ebe0dba2",
+    "url": "/static/css/1.b2c0836d.chunk.css"
+  },
+  {
+    "revision": "c1cdd084918e34298b2d2b65689fd3f5",
+    "url": "/index.html"
+  }
+];
