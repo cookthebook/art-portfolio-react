8bf464ab69c3f932aab28a4b593e04d93ef00e99
Aaron Cook <cookx770@umn.edu>
2019-01-12 11:05:38 -0600

    Updates

--- precache-manifest.c698586b44e436f303795cbc10edf1fc.js
+++ precache-manifest.c698586b44e436f303795cbc10edf1fc.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/art-portfolio-react/static/js/runtime~main.1fc312f8.js"
   },
   {
-    "revision": "e06a7083f1aa1187b3b2",
-    "url": "/art-portfolio-react/static/js/main.e06a7083.chunk.js"
+    "revision": "eddabf08d8f4fbd6d6c6",
+    "url": "/art-portfolio-react/static/js/main.eddabf08.chunk.js"
   },
   {
     "revision": "72f3c5d62e55ebe0dba2",
     "url": "/art-portfolio-react/static/js/1.72f3c5d6.chunk.js"
   },
   {
-    "revision": "e06a7083f1aa1187b3b2",
+    "revision": "eddabf08d8f4fbd6d6c6",
     "url": "/art-portfolio-react/static/css/main.8dbbec05.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/art-portfolio-react/static/css/1.b2c0836d.chunk.css"
   },
   {
-    "revision": "14200a503bca7434337adcfde44d9f87",
+    "revision": "a1e97fb6455dfe2309b499faa3fe602c",
     "url": "/art-portfolio-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/art-portfolio-react/precache-manifest.30f95fd3e5da2130a58dc9f1a7fb979c.js"
+  "/art-portfolio-react/precache-manifest.c698586b44e436f303795cbc10edf1fc.js"
 );
 
 workbox.clientsClaim();
